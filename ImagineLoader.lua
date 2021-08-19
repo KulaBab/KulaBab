7f57f905da9193f31e74fea2ddca8a39d6295476
KulaBab <82053505+KulaBab@users.noreply.github.com>
2021-08-19 23:55:03 +0300

    Create ImagineLoader.lua

---
+++ ImagineLoader.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 5627314498 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KulaBab/KulaBab/main/ImagineHub.lua"))()
+if game.PlaceId == 5739953467 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/KulaBab/KulaBab/main/ImagineHubLobby.lua"))()
+end
+    end
